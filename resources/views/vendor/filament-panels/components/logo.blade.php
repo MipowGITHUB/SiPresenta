045170513f42dfff5d9f46147d59b2b1d3f354d5
putrinaylasabri <putrinaylasabrii@gmail.com>
2025-06-10 10:23:47 +0700

    updated all fingerprint features integrated with rfid

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++
@@ -1,6 +0,0 @@
-<img
-
-    src="{{ asset('images/logo.png')}}"
-    alt="SiPresenta"
-    style="height:64px; width: auto; max-height: 64px; object-fit: contain;"
-/>
--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-Route::delete('/asisten/kelas/{id}', [\App\Http\Controllers\AsistenKelasController::class, 'destroy'])
-    ->name('hapus.kelas.asisten');
-
-Route::get('/', function () {
-    return view('welcome');
-});
